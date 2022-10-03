bc881e4c0e67c498a053c365ee792db6071a358b
JatinNeeraj <113779133+JatinNeeraj@users.noreply.github.com>
2022-10-03 13:35:00 +0530

    neeraj

--- resources/js/KeshavSoft/Ui/BookingFuncs.js
+++ resources/js/KeshavSoft/Ui/BookingFuncs.js
@@ -66,7 +66,11 @@ class KSGlobalBookingClass {
                 },
                 ListenerFuncs: {
                     SaveFunc: async () => {
-                        console.log("this is save func for bookings");
+                        
+                        let LocalGarmentsFirstRow = jFGarmentsFirstRow({KResult})
+
+                        console.log("LocalGarmentsFirstRow",LocalGarmentsFirstRow);
+                        console.log("this is save func for bookings----");
                     },
                     SaveFunc_oldgood: async () => {
                         let jVarLocalFormVertical = document.getElementById("FormVertical");
