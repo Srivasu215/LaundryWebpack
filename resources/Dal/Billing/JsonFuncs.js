e0e5ff8adeb5dec049c948f571a1ef08e731f1c2
Tulasi1920 <100200964+Tulasi1920@users.noreply.github.com>
2022-10-03 13:39:50 +0530

    renamed:    resources/Dal/ToDeliver/CompleteFuns.js -> resources/Dal/Billing/CompleteFuns.js new file:   resources/Dal/Billing/JsonFuncs.js renamed:    resources/Dal/ToDeliver/Show.js -> resources/Dal/Billing/Show.js new file:   resources/Html/Hbs/Billing/Billing.html new file:   resources/Html/Templates/Billing/Header.html new file:   resources/Html/Templates/Billing/Scan.html modified:   resources/Html/Templates/Header.html modified:   resources/dist/bundle.js new file:   resources/Dal/Billing/ScanFuncs.js new file:   resources/js/KeshavSoft/Ui/BillingFuncs.js new file:   resources/js/KeshavSoft/Ui/BillingScanFuncs.js modified:   resources/js/KeshavSoft/Ui/MenuFuncs.js

---
+++ resources/Dal/Billing/JsonFuncs.js
@@ -0,0 +1,83 @@
+import { DalBookingFuncsClass } from "../Bookings/JsonFuncs";
+import _ from "../../js/lodash";
+
+class DalBillingClass {
+
+    static ShowAllFunc = async () => {
+        let LocalJsonFileName = "Completed.json";
+
+        let LocalReturnObject = { KTF: false, KResult: "", JsonData: {} };
+
+        let LocalData = await Neutralino.filesystem.readFile(`./KData/JSON/2017/${LocalJsonFileName}`);
+        let LocalDataAsJson = JSON.parse(LocalData);
+        let LocalCollectionData = Object.keys(LocalDataAsJson).map(key => ({ key, value: LocalDataAsJson[key] }));
+
+        LocalReturnObject.JsonData = LocalCollectionData;
+        LocalReturnObject.KTF = true;
+
+        return await LocalReturnObject;
+    };
+
+    static ShowWithBookingDataFunc = async () => {
+        let LocalReturnObject = { KTF: false, KResult: "", JsonData: {} };
+
+        let LocalBookingData = await DalBookingFuncsClass.ShowAllFunc();
+        let LocalCompletedData = await this.ShowAllFunc();
+
+        if (LocalCompletedData.KTF === false) {
+            LocalReturnObject.KReason = LocalCompletedData.KReason;
+            return await LocalReturnObject;
+        };
+
+        if (LocalBookingData.KTF === false) {
+            LocalReturnObject.KReason = LocalBookingData.KReason;
+            return await LocalReturnObject;
+        };
+
+        LocalReturnObject.JsonData = _.map(LocalCompletedData.JsonData, (LoopItem) => {
+            let LoopInside = _.find(LocalBookingData.JsonData, LoopBooking => {
+                return LoopItem.key in LoopBooking.value.QrCodes;
+            });
+            console.log("LoopInside", LoopInside);
+            if ((LoopInside === undefined) === false) {
+                LoopItem.value.CustomerName = LoopInside.value.CustomerName;
+            } else {
+                LocalReturnObject.KReason = `${LoopItem.key} : Qrcode not found in Bookings.json`;
+            };
+
+            return LoopItem;
+        });
+
+        LocalReturnObject.KTF = true;
+
+        return await LocalReturnObject;
+    };
+
+
+    static CommonFuns = {
+        toNumbers: arr => arr.map(Number),
+        LocalGetDate: () => {
+            let date = new Date();
+
+            let dd = (date.getDate() < 10 ? '0' : '') + date.getDate();
+            let MM = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
+            let yyyy = date.getFullYear();
+            let HH = date.getHours();
+            let mm = date.getMinutes();
+            let ss = date.getSeconds();
+
+            return `${dd}-${MM}-${yyyy}-${HH}-${mm}-${ss}`;
+        },
+        LocalGetDateOnly: () => {
+            let date = new Date();
+
+            let dd = (date.getDate() < 10 ? '0' : '') + date.getDate();
+            let MM = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
+            let yyyy = date.getFullYear();
+
+            return `${dd}-${MM}-${yyyy}`;
+        }
+    }
+};
+
+export { DalBillingClass }
---
+++ resources/js/KeshavSoft/Ui/BillingScanFuncs.js
@@ -0,0 +1,126 @@
+import { DalScanClass } from "../../../Dal/Billing/ScanFuncs";
+
+class KSGlobalBillingScanClass {
+    static JSFuncs = {
+        Scan: async () => {
+            let jVarLocalFormVertical = document.getElementById("FormVertical");
+
+            jVarLocalFormVertical.classList.remove('was-validated');
+            jVarLocalFormVertical.classList.add('novalidate');
+
+            if (jVarLocalFormVertical.checkValidity()) {
+                let PromiseDataFromSave = await DalCompletedClass.Booking.InsertFunc({ inObjectToInsert: serializeObject(jVarLocalFormVertical) });
+
+                if (PromiseDataFromSave.KTF) {
+                    let jVarLocalAlertSuccessId = document.getElementById("AlertSuccessId");
+                    let jVarLocalAlertSuccessMessageId = document.getElementById("AlertSuccessMessageId");
+                    jVarLocalAlertSuccessId.style.display = "";
+                    jVarLocalAlertSuccessMessageId.innerHTML = `<strong>Hurray!</strong> ${PromiseDataFromSave.KResult}`
+
+                    setTimeout(function () {
+                        jVarLocalAlertSuccessId.style.display = "none"
+
+                    }, 7000);
+                } else {
+                    let jVarLocalAlertDangerId = document.getElementById("AlertWarningId");
+                    let jVarLocalAlertDangerMessageId = document.getElementById("AlertWarningMessageId");
+                    jVarLocalAlertDangerId.style.display = "";
+                    jVarLocalAlertDangerMessageId.innerHTML = `<strong>Sorry!</strong> ${PromiseDataFromSave.KReason}`
+
+                    setTimeout(function () {
+                        jVarLocalAlertDangerId.style.display = "none"
+
+                    }, 7000);
+                };
+            } else {
+                document.forms[1].classList.add('was-validated');
+            };
+        },
+    };
+
+    static ApiFuncs = {
+        AddListeners: () => {
+            let jVarLocalGoButtonid = document.getElementById("GoButtonid");
+
+            jVarLocalGoButtonid.addEventListener("click", this.ApiFuncs.GoClick);
+        },
+        GoClick: async () => {
+            let jVarLocalScanId = document.getElementById("ScanId");
+            console.log("jVarLocalScanId  ", jVarLocalScanId.value);
+            let jVarLocalFromInsert = await DalScanClass.InsertFunc({ inQrCode: jVarLocalScanId.value });
+            console.log("jVarLocalFromInsert  ", jVarLocalFromInsert);
+        },
+        CommonFuncs: {
+            AddListeners: () => {
+                let jVarLocalHomeId = document.getElementById("HomeId");
+
+                jVarLocalHomeId.addEventListener("click", async () => {
+                    await KSGlobalMenuClass.ApiFuncs.Header.BuildMenu();
+
+                    let jVarLocalKCont1 = document.getElementById("KCont1");
+                    jVarLocalKCont1.innerHTML = "";
+                });
+                let jVarLocalBillingHeaderId = document.getElementById("BillingHeaderId");
+
+                jVarLocalBillingHeaderId.addEventListener("click", async () => {
+                    await KSGlobalMenuClass.ApiFuncs.Header.BuildMenu();
+
+                    let jVarLocalKCont1 = document.getElementById("KCont1");
+                    jVarLocalKCont1.innerHTML = "";
+                });
+
+            }
+        }
+    };
+
+    static HtmlFuns = {
+        Templates: {
+            Header: {
+                PullFunc: async () => {
+                    let jVarLocalFetchUrl = "Templates/Billing/Header.html";
+                    let response = await fetch(jVarLocalFetchUrl);
+                    let data = await response.text();
+                    return await data;
+                }
+            }
+        },
+        Hbs: {
+
+            Scan: async () => {
+                let jVarLocalFetchUrl = "Hbss/Billing/Scan.html";
+                let response = await fetch(jVarLocalFetchUrl);
+                let data = await response.text();
+                return await data;
+            }
+        }
+    };
+
+    static CommonFuncs = {
+        Ui: {
+            Html: {
+                DOM: {
+                    Header: {
+                        ChangeClass: ({ inHtmlControl }) => {
+                            let jVarLocalClosestUl = inHtmlControl.closest("ul");
+                            let jVarLocalQueryATags = jVarLocalClosestUl.querySelectorAll("a");
+                            let jVarLocalClassName = "text-info";
+                            let jVarLocalWhiteClassName = "text-white";
+
+                            jVarLocalQueryATags.forEach((LoopElement) => {
+                                if (LoopElement.classList.contains(jVarLocalClassName)) {
+                                    LoopElement.classList.remove(jVarLocalClassName);
+                                    LoopElement.classList.add(jVarLocalWhiteClassName);
+                                };
+                            });
+
+                            inHtmlControl.classList.remove(jVarLocalWhiteClassName);
+                            inHtmlControl.classList.add(jVarLocalClassName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+export { KSGlobalBillingScanClass };
