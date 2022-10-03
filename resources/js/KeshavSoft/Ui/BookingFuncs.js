32a3aa707527a2a3f54e34f3ac24d3a7196084a5
JatinNeeraj <113779133+JatinNeeraj@users.noreply.github.com>
2022-10-03 13:35:44 +0530

    jjjj

--- resources/js/KeshavSoft/Ui/BookingFuncs.js
+++ resources/js/KeshavSoft/Ui/BookingFuncs.js
@@ -66,6 +66,7 @@ class KSGlobalBookingClass {
                 },
                 ListenerFuncs: {
                     SaveFunc: async () => {
+
                         
                         let LocalGarmentsFirstRow = jFGarmentsFirstRow({KResult})
 
